182a594f3ece86150678dc9f8e16cb7015453a42
yangqh521 <yangqh521@qq.com>
2019-03-26 20:15:00 +0800

    add log

---
+++ src/main/java/com/yqh/boot/controller/LogAction.java
@@ -0,0 +1,35 @@
+package com.yqh.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yqh.boot.common.AppResponse;
+import com.yqh.boot.log.LogNameType;
+import com.yqh.boot.log.filter.LogWebContext;
+
+@RestController
+@RequestMapping("/log")
+public class LogAction {
+
+
+	@RequestMapping("/test1")
+	public AppResponse logTest1(String username,String password){
+		System.out.println("--->> log test1 start <<---");
+		
+		LogWebContext logWebContext = LogWebContext.getInstance();
+		
+		AppResponse resp = new AppResponse();
+		
+		logWebContext.putLog("username", username);
+		logWebContext.putLog("password", password);
+		logWebContext.putLog("timestamp", System.currentTimeMillis());
+		
+		logWebContext.setLogName(LogNameType.CONSOLE);
+		
+		System.out.println("--->> log test1 end <<---");
+		resp.setData("adcbefg");
+		resp.setCode(0);
+		return resp;
+	}
+	
+}
--- src/main/java/com/yqh/boot/log/filter/LogWebFilter.java
+++ src/main/java/com/yqh/boot/log/filter/LogWebFilter.java
@@ -1,9 +1,7 @@
-package com.yqh.boot.log;
+package com.yqh.boot.log.filter;
 
 import java.io.IOException;
-import java.util.Date;
 import java.util.Map;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -12,53 +10,54 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.core.annotation.Order;
-
+import org.springframework.stereotype.Component;
 import com.alibaba.fastjson.JSONObject;
-import com.yqh.boot.log.aop.LogWebContext;
+import com.yqh.boot.log.LogKeyType;
 
 @Order(1)
+@Component
 @WebFilter(filterName = "logWebFilter", urlPatterns = "/*")
 public class LogWebFilter implements Filter {
 
 	@Override
     public void init(FilterConfig filterConfig) throws ServletException {
-		
+		System.out.println("------>>> LogWebFilter init ~~~");
     }
 	
 	@Override
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 		
-		 HttpServletRequest request = (HttpServletRequest) servletRequest;
+		System.out.println("------>>> LogWebFilter begin ~~~");
+		
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
 		Map<String, String[]> parameterMap = request.getParameterMap();
 		String requestURI = request.getRequestURI();
 		
 		long startTime = System.currentTimeMillis();
 		
 		LogWebContext logWebContext = LogWebContext.getInstance();
-		logWebContext.putLog(LogKeyType.URI.getLogKey(), requestURI);
-		logWebContext.putLog(LogKeyType.PAMRAMS.getLogKey(), JSONObject.toJSONString(parameterMap));
+		logWebContext.getMap().put(LogKeyType.URI.getLogKey(), requestURI);
+		logWebContext.getMap().put(LogKeyType.PAMRAMS.getLogKey(), JSONObject.toJSONString(parameterMap));
 		
-		System.out.println("[ " + requestURI +" ] params:" + JSONObject.toJSONString(parameterMap));
+		System.out.println("[ LogWebFilter " + requestURI +" ] params:" + JSONObject.toJSONString(parameterMap));
 		
 		// 向下执行
 		filterChain.doFilter(servletRequest, servletResponse);
 		
-		HttpServletResponse response = (HttpServletResponse) servletResponse;
+//		HttpServletResponse response = (HttpServletResponse) servletResponse;
+		
 		String costTime = (System.currentTimeMillis() - startTime) + "ms";
-		
-		logWebContext.putLog(LogKeyType.COST_TIME.getLogKey(), costTime);
-		
+		logWebContext.getMap().put(LogKeyType.COST_TIME.getLogKey(), costTime);
 		logWebContext.printLog();
+		System.out.println("[ LogWebFilter " + requestURI +" ] cost:" + costTime);
 		logWebContext.clear();
-		System.out.println("[ " + requestURI +" ] cost:" + costTime);
+		System.out.println("------>>> LogWebFilter end ~~~");
 	}
 	
 	@Override
     public void destroy() {
-		
+		System.out.println("------>>> LogWebFilter destroy !!!");
     }
 
 }
